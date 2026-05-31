4353f8c2974617cd363e82a85f596af0110ba87a
Aaryan Pashine <pashineaaryan@gmail.com>
2026-05-31 19:38:38 -0400

    added full screen

--- script.js
+++ script.js
@@ -7,9 +7,95 @@ let pckry
 
 let projectMedia = document.querySelector('.project-media')
 let projectMetadata = document.querySelector('.project-metadata')
+let overlay = document.querySelector(".overlay")
+
+
+
+function resetOverlay() {
+	overlay.innerHTML = ''
+	overlay.style.display = 'none'
+}
+
+overlay.onclick = resetOverlay
 
 let included  = {}
 let cleaned 
+
+// source ~ https://gist.github.com/kjbrum/2c784eda82f880e21cb3f0d621411e44
+const video = {
+    parse: function(url) {
+        // - Supported YouTube URL formats:
+        //   - http://www.youtube.com/watch?v=My2FRPA3Gf8
+        //   - http://youtu.be/My2FRPA3Gf8
+        //   - https://youtube.googleapis.com/v/My2FRPA3Gf8
+        // - Supported Vimeo URL formats:
+        //   - http://vimeo.com/25451551
+        //   - http://player.vimeo.com/video/25451551
+        // - Also supports relative URLs:
+        //   - //player.vimeo.com/video/25451551
+
+        url.match(/(http:|https:|)\/\/(player.|www.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/);
+
+        if (RegExp.$3.indexOf('youtu') > -1) {
+            var type = 'youtube';
+        } else if (RegExp.$3.indexOf('vimeo') > -1) {
+            var type = 'vimeo';
+        }
+
+        return {
+            type: type,
+            id: RegExp.$6
+        };
+    },
+
+    // Returns an iframe of the video with the specified URL.
+    createEmbed: function(url, width, height) {
+        var self = this;
+        var videoObj = self.parse(url);
+
+        var iframe = document.createElement('iframe');
+        iframe.width = width || 560;
+        iframe.height = height || 315;
+        iframe.frameborder = 0;
+        iframe.setAttribute('allowFullScreen', '');
+
+        if (videoObj.type == 'youtube') {
+            iframe.src = '//www.youtube.com/embed/' + videoObj.id;
+            iframe.allow = "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture";
+        } else if (videoObj.type == 'vimeo') {
+            iframe.src = '//player.vimeo.com/video/' + videoObj.id;
+            iframe.allow = "autoplay; fullscreen";
+        }
+
+        return iframe;
+    },
+
+    // Obtains the video's thumbnail and passed it back to a callback function.
+    getThumbnail: function(url, cb) {
+        var self = this;
+        var videoObj = self.parse(url);
+        var thumbUrl;
+
+        switch (videoObj.type) {
+            case 'youtube':
+                thumbUrl = '//img.youtube.com/vi/' + videoObj.id + '/maxresdefault.jpg';
+                break;
+            case 'vimeo':
+                thumbUrl = '//i.vimeocdn.com/video/' + videoObj.id + '_640.jpg';
+                break;
+            default:
+                thumbUrl = '';
+                break;
+        }
+
+        return thumbUrl;
+    },
+};
+
+function init(){
+
+}
+
 
 fetch(link, {
 	Accept: 'application/json',
@@ -18,15 +104,16 @@ fetch(link, {
 })
 	.then(res => res.json())
 	.then(res => {
-		console.log(res.data)
-		console.log(res)
 		data = res.data
+
 		res.included.forEach(e => {
 			included[e.id] = e
 		})
 
 		cleaned = (res.data.map(x => {
 			let attr = x.attributes
+			let tags = x.relationships.field_tags.data
+			console.log(tags)
 			let media = x.relationships.field_media_gallery.data
 			let thumbnail = x.relationships.field_thumbnail_image?.data
 			if (thumbnail){
@@ -61,21 +148,23 @@ fetch(link, {
 			// console.log(images)
 
 			let videos = x.relationships.field_media_gallery
-			console.log(videos.data.filter(e => e.type == 'paragraph--video').map(e => included[e.id].attributes))
+			let videoObjects = videos.data.filter(e => e.type == 'paragraph--video').map(e => included[e.id].attributes)
+			if (videoObjects.length > 0) console.log("Has video", videoObjects.map(e => e.field_video_url ? video.parse(e.field_video_url?.uri) : ''))
+
+			// console.log(videos.data.filter(e => e.type == 'paragraph--video').map(e => included[e.id].attributes))
 
 			let o = {}
 			o.projectTitle = attr.title
 			o.firstName = attr.field_first_name_preferred_names
 			o.lastName = attr.field_last_name
-			console.log(attr)
 			o.description = attr.field_project_description.value
 			o.bio = attr.field_short_biography?.value
 			o.portfolioLink = attr.field_portfolio_site_link?.uri
 			o.instagramLink = attr.field_instagram_link?.uri
 			o.images = images
+			o.videos = videoObjects
 			o.thumbnail = thumbnail
 			o.id = x.id
-			// console.log(images)
 			if (!o.thumbnail && images && images.length > 0) o.thumbnail = images[0]
 
 			return o
@@ -141,9 +230,12 @@ function initPackery() {
 		destroyPackery()
 		return
 	}
+
 	const grid = document.querySelector('.grid-container')
 	if (!grid) return
+
 	destroyPackery()
+
 	pckry = new Packery(grid, {
 		gutter: -30,
 		itemSelector: '.grid-item'
@@ -175,6 +267,7 @@ function openProfile(id) {
 	projectMetadata.style.display = 'block'
 	projectMedia.innerHTML = ''
 	appendProjectImages(id)
+
 }
 
 function appendProjectImages(id){
@@ -182,13 +275,27 @@ function appendProjectImages(id){
 
 	project.images.forEach(item => {
 		let img = document.createElement("img")
+		let fullscreen = document.createElement("img")
 		img.src = item.url
+		fullscreen.src = item.url
+
 		img.classList.add('project-img')
 		img.style.opacity = 0
+
+		img.onclick = () => {
+			overlay.style.display = 'flex'
+			overlay.appendChild(fullscreen)
+		}
+
 		projectMedia.appendChild(img)
 		setTimeout(() => {
 			img.style.opacity=1
 		}, random(500, 1500))
+	})
+
+	project.videos.forEach(item => {
+		console.log(item.field_video_url)
+		projectMedia.appendChild(video.createEmbed(item.field_video_url.uri, "100%", '600px'))
 	})
 
 	projectMetadata.innerHTML = `
@@ -213,13 +320,9 @@ ${project.portfolioLink || project.instagramLink ? `
 </div>
 ` : ''}
 `
-
-
 	setTimeout(() => {
 		projectMetadata.style.opacity=1
 	}, random(5, 15))
-
-	// console.log(project.ima)
 
 }
 
@@ -260,8 +363,6 @@ function initHomePage(items) {
 		let height = ratio * (item.thumbnail?.height ? item.thumbnail.height : w)
 
 		if (item.projectTitle == 'Cura Prototype') return ''
-
-		console.log(imageStyleUrl(item.thumbnail.url, 'thumbnail'));
 
 		return `
 <a href='#${item.id}'>
@@ -324,4 +425,10 @@ window.addEventListener('resize', () => {
 		pckry.reloadItems()
 		pckry.layout()
 	}, 180)
+})
+
+window.addEventListener('keydown', (e) => {
+	if (e.key == 'Escape' && overlay.style.display == 'flex'){
+		resetOverlay()
+	}
 })
